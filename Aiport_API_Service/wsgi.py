ab0f8f3aa8eb574c1a0cfedb334ae2b81390847d
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-09-04 11:17:35 +0300

    change KEY

--- Aiport_API_Service/wsgi.py
+++ Aiport_API_Service/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aiport_API_Service.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Aiport_API_Service.settings")
 
 application = get_wsgi_application()
